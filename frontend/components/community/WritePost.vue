f09caa286307d34f176a3e3f5a6ad7756ebbcf32
isho-0 <isho010607@gmail.com>
2025-10-12 11:47:24 +0900

    feat: 이미지 업로드 템플릿

--- frontend/components/community/WritePost.vue
+++ frontend/components/community/WritePost.vue
@@ -22,3 +22,29 @@
           />
         </div>
 
+        <!-- 이미지 업로드 -->
+        <div class="form-group">
+          <label>이미지</label>
+          <div class="image-upload-area">
+            <input
+              ref="imageInput"
+              type="file"
+              accept="image/*"
+              @change="handleImageSelect"
+              style="display: none"
+            />
+            
+            <div v-if="!imagePreview" class="upload-placeholder" @click="triggerImageUpload">
+              <i class="material-icons">add_photo_alternate</i>
+              <p>이미지를 추가하세요 (선택사항)</p>
+            </div>
+            
+            <div v-else class="image-preview">
+              <img :src="imagePreview" alt="Preview" />
+              <button type="button" class="remove-image-btn" @click="removeImage">
+                <i class="material-icons">close</i>
+              </button>
+            </div>
+          </div>
+        </div>
+
