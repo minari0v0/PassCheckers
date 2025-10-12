8c5f737590d8b4aca3cb6bacd7dc92b852557a6d
isho-0 <isho010607@gmail.com>
2025-10-12 11:46:54 +0900

    feat: 게시글 작성 및 제목 템플릿

---
+++ frontend/components/community/WritePost.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="write-post-modal">
+    <div class="modal-content" @mousedown.stop @touchstart.stop>
+      <div class="modal-header">
+        <h2>게시글 작성</h2>
+        <button class="close-btn" @click="closeModal">
+          <i class="material-icons">close</i>
+        </button>
+      </div>
+
+      <form @submit.prevent="submitPost" class="post-form">
+        <!-- 제목 -->
+        <div class="form-group">
+          <label for="title">제목 <span class="required">*</span></label>
+          <input
+            id="title"
+            v-model="formData.title"
+            type="text"
+            placeholder="제목을 입력하세요"
+            required
+            maxlength="255"
+          />
+        </div>
+
