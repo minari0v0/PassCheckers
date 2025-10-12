52db155c641086c9e09f016f8058f40b458549a1
isho-0 <isho010607@gmail.com>
2025-10-13 00:05:47 +0900

    feat: 프로덕션 모드 배포 기능

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   modules: ["nuxt-quasar-ui"],
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   css: [
     '~/assets/main.css'
   ],
